6e783a7f336878bc6c5bd706121561e338343846
Alpha-T30 <mrhaquet20@gmail.com>
2021-09-22 16:05:29 +0600

    spinner added

--- src/ContextApi/UserContaxt/AuthContext.js
+++ src/ContextApi/UserContaxt/AuthContext.js
@@ -4,20 +4,20 @@ import { useEffect } from "react";
 
 const initialState = {
   user: JSON.parse(localStorage.getItem("user")) || null,
-  isFeching: false,
+  isFetching: false,
   error: false,
 };
 
 export const AuthContext = createContext(initialState);
 
 export const AuthContextProvider = ({ children }) => {
-  // user:state.user, isFetching:state.isFetching, error:state.error
   const [state, sender] = useReducer(authReducer, initialState);
   useEffect(() => {
     localStorage.setItem("user", JSON.stringify(state.user));
   }, [state.user]);
   return (
-    <AuthContext.Provider value={{ ...state, sender }}>
+    <AuthContext.Provider value={{  user:state.user, isFetching:state.isFetching, error:state.error
+      , sender }}>
       {children}
     </AuthContext.Provider>
   );
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { updateUser } from "../../redux/apiCalls";
 import { AuthContext } from "../../ContextApi/UserContaxt/AuthContext";
 import { login } from "../../ContextApi/UserContaxt/apiCalls";
+import CircularProgress from '@mui/material/CircularProgress';
 
 export default function Login() {
 
@@ -14,8 +15,8 @@ export default function Login() {
 
   // const dispatch = useDispatch();
 
-  const { sender:dispatch ,isFeching, error} = useContext(AuthContext);
-console.log(error)
+  const { sender:dispatch ,isFetching, error} = useContext(AuthContext);
+console.log(isFetching)
   const formRef = useRef();
   const handleSubmit=(e)=>{
     e.preventDefault() ; 
@@ -26,8 +27,8 @@ console.log(error)
 
   }
  
- 
   return (
+    !isFetching?
     <div className={Style.login}>
       <div className={Style.wrapper}>
         <img src="/Images/logo1.jpg" alt="" />
@@ -35,13 +36,25 @@ console.log(error)
         <form onSubmit={handleSubmit} ref={formRef} >
           <input placeholder="User Id" type="text" name="username" id="username" />
           <input placeholder="Password" type="password" name="password" />
-          <button type='submit'>Log In</button>
+          <button  type='submit'>Log In</button>
 
         </form>
         <div className={Style.promotion}>
             <span>Powered By <a href="https://www.facebook.com">Bangla Pay</a></span>
         </div>
       </div>
+    </div> :  (
+      <div className={Style.isFetching}>
+      
+      <div class="divLoader">
+      <p>Logging In ....</p>
+      <svg class="svgLoader" viewBox="0 0 100 100" width="10em" height="10em">
+        <path ng-attr-d="{{config.pathCmd}}" ng-attr-fill="{{config.color}}" stroke="none" d="M10 50A40 40 0 0 0 90 50A40 42 0 0 1 10 50" fill="#51CACC" transform="rotate(179.719 50 51)"><animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 51;360 50 51" keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animateTransform></path>
+      </svg>
+      <button onClick={()=>window.location.reload()}>Try Again</button>
     </div>
-  );
+      </div>
+    )
+  )  
+  
 }
